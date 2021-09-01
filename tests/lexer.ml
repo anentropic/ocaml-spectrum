c74acf842bc413951e599bf4d374531762580e8d
Anentropic <ego@anentropic.com>
2021-09-01 11:47:03 +0100

    support compound (style + color) shorthand tags

--- tests/lexer.ml
+++ tests/lexer.ml
@@ -19,6 +19,11 @@ let () =
           test_case "Foreground (explicit): dark-olive-green-1a" `Quick (test_tag_to_code "fg:dark-olive-green-1a" "38;5;191");
           test_case "Background (explicit): dark-olive-green-1a" `Quick (test_tag_to_code "bg:dark-olive-green-1a" "48;5;191");
         ];
+      "Compound from named color", [
+          test_case "Red, Underline" `Quick (test_tag_to_code "red:underline" "4;38;5;1");
+          test_case "Bg Red, Underline" `Quick (test_tag_to_code "bg:red:underline" "4;48;5;1");
+          test_case "dark-olive-green-1a, Bold" `Quick (test_tag_to_code "dark-olive-green-1a:bold" "1;38;5;191");
+        ];
       "Hex colors", [
           test_case "Foreground (implicit): fc9" `Quick (test_tag_to_code "#fc9" "38;2;255;204;153");
           test_case "Foreground (explicit): fc9" `Quick (test_tag_to_code "fg:#fc9" "38;2;255;204;153");
@@ -32,5 +37,9 @@ let () =
           test_case "Foreground (implicit): F0C090" `Quick (test_tag_to_code "#F0C090" "38;2;240;192;144");
           test_case "Foreground (explicit): F0C090" `Quick (test_tag_to_code "fg:#F0C090" "38;2;240;192;144");
           test_case "Background (explicit): F0C090" `Quick (test_tag_to_code "bg:#F0C090" "48;2;240;192;144");
+        ];
+      "Compound from hex color", [
+          test_case "#F00, Underline" `Quick (test_tag_to_code "#F00:underline" "4;38;2;255;0;0");
+          test_case "Bg #f0c090, Bold" `Quick (test_tag_to_code "bg:#f0c090:bold" "1;48;2;240;192;144");
         ];
     ]
