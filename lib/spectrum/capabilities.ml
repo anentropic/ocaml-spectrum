d3c3ba8c93473e030778f92b91d6554fa3da3dfd
Anentropic <ego@anentropic.com>
2022-01-23 09:03:22 +0000

    WIP

--- lib/spectrum/capabilities.ml
+++ lib/spectrum/capabilities.ml
@@ -197,6 +197,7 @@ end
 
 module Sys_Capabilities = Make(Sys)(SysOsInfo)
 
+(* default export *)
 include Sys_Capabilities
 
 type color_level_info = {
--- lib/spectrum_tools/utils.ml
+++ lib/spectrum_tools/utils.ml
@@ -1,5 +1,72 @@
+(* float division of integers *)
+let (//) a b = float_of_int a /. float_of_int b
+
 (* rounds to nearest, .5 rounds up, -.5 rounds down *)
 let int_round n = Float.round n |> int_of_float
+
+let map_color f (color : Color.Rgba.t) = (f color.r), (f color.g), (f color.b)
+
+let product3 l l' l'' = 
+  List.concat_map (fun e ->
+      List.concat_map (fun e' ->
+          List.map (fun e'' -> (e, e', e'')) l'') l') l
+
+
+module type Showable = sig
+  type t
+  val pp : Format.formatter -> t -> unit
+  val show : t -> string
+end
+
+module type OrderedShowable = sig
+  include Set.OrderedType
+  include Showable with type t := t
+end
+
+module ShowableSet = struct
+  include Set
+  module type S = sig
+    include Set.S
+  end
+  module Make (Ord : OrderedShowable) = struct
+    include Set.Make(Ord)
+  end
+end
+
+module type AdjacencySet = sig
+  include ShowableSet.S
+  val adjacent_values : t -> elt -> elt list option
+  val adjacent_values_exn : t -> elt -> elt list
+end
+
+module AdjacencySet_Make (El : OrderedShowable) : AdjacencySet with type elt = El.t = struct
+  include ShowableSet.Make(El)
+
+  let adjacent_values set value =
+    match split value set with
+    | _, true, _ -> Some [value]
+    | lt, false, gt -> begin
+        match max_elt_opt lt, min_elt_opt gt with
+        | Some prev, Some next -> Some [prev; next]
+        | _, None -> None  (* value is > all elements *)
+        | None, _ -> None  (* value is < all elements *)
+      end
+
+  let adjacent_values_exn set value =
+    match adjacent_values set value with
+    | Some l -> l
+    | None -> invalid_arg @@ El.show value
+end
+
+module Int' = struct
+  include Int
+  type t = int [@@deriving show]
+end
+
+module IntAdjacencySet = AdjacencySet_Make(Int')
+
+
+(* ---- UNUSED scratch pad ---- *)
 
 let rgb_int_of_srgb_component x =
   (*
@@ -11,11 +78,11 @@ let rgb_int_of_srgb_component x =
   to avoid this we copy some logic from:
   https://github.com/dbuenzli/gg/blob/master/src/gg.ml#L2714
   *)
-  let c0 = 0.0031308 in
-  let c1 = 12.92 in
-  let c2 = 1.055 in
-  let c3 = 1. /. 2.4 in
-  let c4 = 0.055 in
+  let c0 = 0.0031308
+  and c1 = 12.92
+  and c2 = 1.055
+  and c3 = 1. /. 2.4
+  and c4 = 0.055 in
   let x' = if x <= c0 then c1 *. x else c2 *. (x ** c3) -. c4 in
   int_round (x' *. 255.)
 
@@ -74,3 +141,20 @@ let range ?(from=0) until ?(step=1) =
   Seq.unfold (function
         i when cmp i until -> Some (i, i + step) | _ -> None
     ) from
+
+(* enumerate all RGB values, as v4 vector colour *)
+let rgb_seq ?(rmax=256) ?(gmax=256) ?(bmax=256) =
+  Seq.flat_map (fun r ->
+      Seq.flat_map (fun g ->
+          Seq.map (fun b ->
+              Color.of_rgb r g b
+            ) (range bmax)
+        ) (range gmax)
+    ) (range rmax)
+
+(*
+  find nearest y, where x=2^y
+  could be used to determine the Int.shift_right from a grey_threshold in
+  the Chalk rgb_to_ansi256 algorithm 
+*)
+let power_of_2 x = log (float_of_int x) /. log 2. |> int_of_float
